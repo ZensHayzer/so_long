201b847e9141f685b0c9e9708cd5b8c8fd950ce8
ajeanne <ajeanne@student.42.fr>
2022-12-16 20:19:10 +0100

    Escape bug fixed

--- srcs/player.c
+++ srcs/player.c
@@ -6,7 +6,7 @@
 /*   By: ajeanne <ajeanne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 22:30:09 by ajeanne           #+#    #+#             */
-/*   Updated: 2022/12/16 18:50:54 by ajeanne          ###   ########.fr       */
+/*   Updated: 2022/12/16 20:17:29 by ajeanne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ int	p_up(t_map_content *map_c)
 	if (map_c->map[map_c->player_y - 1][map_c->player_x] != '1')
 	{
 		if (map_c->map[map_c->player_y - 1][map_c->player_x] == 'C')
-			map_c->items_got = map_c->items_got - 1;
+			map_c->items_got = map_c->items_got + 1;
 		if (map_c->map[map_c->player_y - 1][map_c->player_x] == 'E')
 			if (map_c->items_got == map_c->items)
 				free_closing(map_c, &map_c->vars);
+		if (map_c->last_pos == 'E')
+			map_c->map[map_c->player_y][map_c->player_x] = 'E';
+		else
+			map_c->map[map_c->player_y][map_c->player_x] = '0';
 		map_c->last_pos = map_c->map[map_c->player_y - 1][map_c->player_x];
 		map_c->map[map_c->player_y - 1][map_c->player_x] = 'P';
-		if (map_c->last_pos == '0' || map_c->last_pos == 'C')
-			map_c->map[map_c->player_y][map_c->player_x] = '0';
-		else
-			map_c->map[map_c->player_y][map_c->player_x] = 'E';
 		map_c->player_y = map_c->player_y - 1;
 		return (1);
 	}
@@ -42,12 +42,12 @@ int	p_left(t_map_content *map_c)
 		if (map_c->map[map_c->player_y][map_c->player_x - 1] == 'E')
 			if (map_c->items_got == map_c->items)
 				free_closing(map_c, &map_c->vars);
+		if (map_c->last_pos == 'E')
+			map_c->map[map_c->player_y][map_c->player_x] = 'E';
+		else
+			map_c->map[map_c->player_y][map_c->player_x] = '0';
 		map_c->last_pos = map_c->map[map_c->player_y][map_c->player_x - 1];
 		map_c->map[map_c->player_y][map_c->player_x - 1] = 'P';
-		if (map_c->last_pos == '0' || map_c->last_pos == 'C')
-			map_c->map[map_c->player_y][map_c->player_x] = '0';
-		else
-			map_c->map[map_c->player_y][map_c->player_x] = 'E';
 		map_c->player_x = map_c->player_x - 1;
 		return (1);
 	}
@@ -63,12 +63,12 @@ int	p_down(t_map_content *map_c)
 		if (map_c->map[map_c->player_y + 1][map_c->player_x] == 'E')
 			if (map_c->items_got == map_c->items)
 				free_closing(map_c, &map_c->vars);
+		if (map_c->last_pos == 'E')
+			map_c->map[map_c->player_y][map_c->player_x] = 'E';
+		else
+			map_c->map[map_c->player_y][map_c->player_x] = '0';
 		map_c->last_pos = map_c->map[map_c->player_y + 1][map_c->player_x];
 		map_c->map[map_c->player_y + 1][map_c->player_x] = 'P';
-		if (map_c->last_pos == '0' || map_c->last_pos == 'C')
-			map_c->map[map_c->player_y][map_c->player_x] = '0';
-		else
-			map_c->map[map_c->player_y][map_c->player_x] = 'E';
 		map_c->player_y = map_c->player_y + 1;
 		return (1);
 	}
@@ -84,12 +84,12 @@ int	p_right(t_map_content *map_c)
 		if (map_c->map[map_c->player_y][map_c->player_x + 1] == 'E')
 			if (map_c->items_got == map_c->items)
 				free_closing(map_c, &map_c->vars);
+		if (map_c->last_pos == 'E')
+			map_c->map[map_c->player_y][map_c->player_x] = 'E';
+		else
+			map_c->map[map_c->player_y][map_c->player_x] = '0';
 		map_c->last_pos = map_c->map[map_c->player_y][map_c->player_x + 1];
 		map_c->map[map_c->player_y][map_c->player_x + 1] = 'P';
-		if (map_c->last_pos == '0' || map_c->last_pos == 'C')
-			map_c->map[map_c->player_y][map_c->player_x] = '0';
-		else
-			map_c->map[map_c->player_y][map_c->player_x] = 'E';
 		map_c->player_x = map_c->player_x + 1;
 		return (1);
 	}
@@ -101,6 +101,7 @@ int	player_move(char c, t_map_content *map_c)
 	int	ret;
 
 	ret = 0;
+	printf("%d\n", map_c->items_got);
 	if (c == 'w')
 		ret = p_up(map_c);
 	else if (c == 'a')
